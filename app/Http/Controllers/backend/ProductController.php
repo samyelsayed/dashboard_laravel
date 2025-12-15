c6e7e2c59e7e32aef3c6d098ff8ca353232394b2
samyelsayed <samyelsayed754@gmail.com>
2025-12-15 11:13:15 +0200

    Add validation to admin edit product page

--- app/Http/Controllers/backend/ProductController.php
+++ app/Http/Controllers/backend/ProductController.php
@@ -33,14 +33,11 @@ class ProductController extends Controller
                $subcategories = DB::table('subcategories')->select('id','name_en')->Where('status','=',1)->get();
         return view('backend.product.edit',compact('product','brands','subcategories'));
     }
-//الريكويست هيستقبل ايه ريكويست سواء جيت او بوست او اين كان وعلشان هوا كلاس علشان اتعامل معاه لازم اخد منه اوبجكت
-    function update(Request $request , $id){
-dd($request->all());
-    }
 
 
 
-    //كأني بقوله الفريبول الي اسمه ريكويست دا اوجت من كلاس الريكويست وكدا هوا شايل كل البيانات الي جيت في الفورم
+
+           //كأني بقوله الفريبول الي اسمه ريكويست دا اوجت من كلاس الريكويست وكدا هوا شايل كل البيانات الي جيت في الفورم
             function store(Request $request){
              $rules= [
                 'name_en'=>['required','string','max:256','min:2'], //بكتب اسم ال انبوت الي انا مسميه في الفورم و بحط الماكسيمم الي انا انا حاطة ف يالدا تا بيز و المينمم ولو مش محددها في الدات ابيز فدي حاجة ترجعلي
@@ -79,5 +76,27 @@ dd($request->all());
                     return redirect()->route('products.index');
                 }
     }
+            //الريكويست هيستقبل ايه ريكويست سواء جيت او بوست او اين كان وعلشان هوا كلاس علشان اتعامل معاه لازم اخد منه اوبجكت
+            function update(Request $request , $id){
+              //validation
+                    $rules= [
+                        'name_en'=>['required','string','max:256','min:2'], //بكتب اسم ال انبوت الي انا مسميه في الفورم و بحط الماكسيمم الي انا انا حاطة ف يالدا تا بيز و المينمم ولو مش محددها في الدات ابيز فدي حاجة ترجعلي
+                        'name_ar'=>['required','string','max:256','min:2'],
+                        'price'=>['required','numeric','max:99999.99','min:0.5'],
+                        'code'=>['required','integer','digits:5',"unique:products,code, $id .id"],  //علشان لما اعمل ابديت ومش عاوز يطلعلي ان الكود مكرر مع انة نفس الكود القديم فبديله القيمة الي لو جتله يعملها اجنور يعني يتخطاها و هيا ف انه عمود
+                        'quantity'=>['nullable','integer','max:999','min:1'],  //nullable يعني تقبل انك تكتب فيها null واحنا في الداتا بيز كاتبين ان الديفولت بتاعها 1
+                        'desc_en'=>['required','string'],
+                        'desc_ar'=>['required','string'],
+                        'status'=>['required','string','between:0,1'],
+                        'subcategories_id'=>['required','integer','exists:subcategories,id'],  //بقوله ان الساب كاتجوري ايدي ال يهيجيلك لازم ييبقا موجود ف يجدول الصاب كاتجوري في عمود ال اي دي
+                        'brand_id'=>['required','integer','exists:brands,id'],
+                        'image'=>['nullable','max:1000','mimes:png,jpg,jpeg']  //ميمز بكتب الاكستنشن المسموح بية للصورة و ماكسيمم بكتب في اقصي حجم ليهها بالكيلو بايت
+                    ];
+                    $request->validate($rules);
+              // is photo exists=> upload image
+              //علشان اتشك هل اليوزر رفع او عدل الصورة ولا لا بتشك علي كي ال ايمج  كان مكن اعمل كدا من خلا ل فانشكن از ست بس لارافيل فيها هيلبر اسمه هاذ
+              // update database
+              //redirect
+            }
 
 }
