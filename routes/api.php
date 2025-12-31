6a1b2151f76674011209b18d013fd6f96f27688f
samyelsayed <samyelsayed754@gmail.com>
2025-12-31 12:00:55 +0200

    Manually configured API routing and added api.php

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Apis\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// جرب إضافة هذا المسار للتأكد من عمل الـ API
+Route::get('/test', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'API is working correctly ya smsm!'
+    ]);
+});
+
+// Route::get('/test', function () {
+//     return response()->json(['status' => 'success', 'message' => 'API is working!']);
+// });
+
+
+Route::group(['prefix' => 'products'],function () {
+  Route::get('/',[ProductController::class,'index']);
+  Route::get('create',[ProductController::class,'create']);
+  Route::get('edit/{id}',[ProductController::class,'edit']);
+
+});
