168ce6e675b12ce1043446d093a13a176b66c785
samyelsayed <samyelsayed754@gmail.com>
2026-01-01 10:46:05 +0200

    Added Brand and Subcategory models and cleaned up migrations

--- app/Http/Controllers/Apis/ProductController.php
+++ app/Http/Controllers/Apis/ProductController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers\Apis;
 
-use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Product;
+use App\Models\Subcategory;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ProductController extends Controller
 {
     public function index(){
+        $products = Product::all();
+        return response()->json(compact('products'),200);
+    }
 
+
+    public function create(){
+        $brands = Brand::all();
+        $Subcategories = Subcategory::select('id','name_en')->get();
+        return response()->json(compact('brands', 'Subcategories'),200);
+    }
+
+    public function edit($id){
+        // $products = Product::where('id',$id)->first();
+        $brands = Brand::all();
+        $Subcategories = Subcategory::select('id','name_en')->get();
+        // $products = Product::findOrFail($id);       //لو ما لقاها يرمي ايرور
+        $products = Product::find($id);             //فايند يعني واحد بس
+
+        return response()->json(compact('products', 'brands', 'Subcategories'),200);
     }
 }
